140102a6efa43409ee188d3ac8bd3c8406bbebd4
platon <h46261672@gmail.com>
2024-12-08 15:56:57 +0500

    2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,15 +4,15 @@ import './Main.css'; // Импортируйте файл стилей
 export const Main = () => {
   return (
     <div className="main-container">
-      <h1>Добро пожаловать!</h1>
-      {/* Здесь будет ваша картинка */}
-      <img src={require('./Bag.png')} alt="Описание изображения" className="main-image" />
-      <img src={require('./clock.png')} alt="Описание изображения" className="main-image" />
-      <img src={require('./Jacket.png')} alt="Описание изображения" className="main-image" />
-      <img src={require('./Shoes2.png')} alt="Описание изображения" className="main-image" />
-      <img src={require('./Shoes.png')} alt="Описание изображения" className="main-image" />
-      <img src={require('./Jeans.png')} alt="Описание изображения" className="main-image" />
+      <h1 className="welcome-title">Добро пожаловать!</h1>
+      <div className="images-container">
+        <img src={require('./Bag.png')} alt="Описание изображения" className="main-image" />
+        <img src={require('./clock.png')} alt="Описание изображения" className="main-image" />
+        <img src={require('./Jacket.png')} alt="Описание изображения" className="main-image" />
+        <img src={require('./Shoes2.png')} alt="Описание изображения" className="main-image" />
+        <img src={require('./Shoes.png')} alt="Описание изображения" className="main-image" />
+        <img src={require('./Jeans.png')} alt="Описание изображения" className="main-image" />
+      </div>
     </div>
   );
 };
-
